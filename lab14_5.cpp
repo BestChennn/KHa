7a6e1d18847aa44a8c8de39eb4f39dc94ca653bc
BestChennn <bestchenbestchen@gmail.com>
2021-02-02 18:51:17 +0700

    MNOP

--- lab14_5.cpp
+++ lab14_5.cpp
@@ -3,19 +3,68 @@
 #include <ctime>
 using namespace std;
 
-void shuffle(int &,int &, int &, int &); //Modify input arguments to pointer 
+void shuffle(int *,int *,int *,int *);
 
 int main(){
-	int a = 50, b = 100, c = 500, d = 1000;
+	int m = 50, n = 100, o = 500, p = 1000;
 	
 	srand(time(0));	
 	
 	for(int i = 0;i < 10;i++){
-	    shuffle(a,b,c,d); //Modify input arguments to pointer 
-	    cout << a << " " << b << " " << c << " " << d << "\n";
+	    shuffle(&m,&n,&o,&p); 
+	    cout << m << " " << n << " " << o << " " << p << "\n";
 	}
 	
 	return 0;
 }
 
-//Write definition of shuffle() using pointer here 
+void shuffle(int *m,int *n,int *o,int *p){
+	int N=4;
+	for(int i=0;i<=N;i++){
+		int q = rand()%12+1;
+		switch(q)
+		{
+			case 1:
+				swap(*m,*n);
+				break;
+			case 2:
+				swap(*m,*o);
+				break;
+			case 3:
+				swap(*m,*p);
+				break;
+			case 4:
+				swap(*n,*m);
+				break;
+			case 5:
+				swap(*n,*o);
+				break;
+			case 6:
+				swap(*n,*p);
+				break;
+			case 7:
+				swap(*o,*m);
+				break;
+			case 8:
+				swap(*o,*n);
+				break;
+			case 9:
+				swap(*o,*p);
+				break;
+			case 10:
+				swap(*p,*m);
+				break;
+			case 11:
+				swap(*p,*n);
+				break;
+			case 12:
+				swap(*p,*o);
+				break;
+		}
+	}
+}
+void swap(int &a,int &b){
+	int kak =a;
+	a=b;
+	a=kak;
+}
